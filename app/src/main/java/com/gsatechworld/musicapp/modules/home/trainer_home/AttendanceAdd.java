95be6a4ae3da1421d27b2fa8fd570f51bd7b7bbc
Jinshad <jinshad@gsatechworld.co.in>
2020-06-04 17:29:36 +0530

    Ui changed

---
+++ app/src/main/java/com/gsatechworld/musicapp/modules/home/trainer_home/AttendanceAdd.java
@@ -0,0 +1,14 @@
+package com.gsatechworld.musicapp.modules.home.trainer_home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AttendanceAdd extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_attendance_add);
+    }
+}
---
+++ app/src/main/java/com/gsatechworld/musicapp/modules/select_subcategory/add_subcategory/AddSubCategoryViewModel.java
@@ -0,0 +1,4 @@
+package com.gsatechworld.musicapp.modules.select_subcategory.add_subcategory;
+
+public class AddSubCategoryViewModel {
+}
